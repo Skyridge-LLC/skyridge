a7f792c69c3af83f7224a79c3b2caf88ed8ce942
Michael Wu <michael.yixuan.wu@gmail.com>
2015-04-03 13:35:37 -0400

    listen on 0.0.0.0

--- server.js
+++ server.js
@@ -3,5 +3,5 @@ var compress = require('compression');
 var app = express();
 app.use(compress());
 app.use(express.static(__dirname));
-app.listen(process.env.PORT || 3000);
+app.listen(process.env.PORT || 3000, '0.0.0.0');
 
