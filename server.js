752a47e33cb676dcb0efa19fca7cfa4ce42d3d8e
Michael Wu <michael.yixuan.wu@gmail.com>
2015-07-08 18:00:02 -0400

    created small expressjs server to serve page

---
+++ server.js
@@ -0,0 +1,6 @@
+var express = require('express');
+var compress = require('compression');
+var app = express();
+app.use(compress());
+app.use(express.static(__dirname));
+app.listen(process.env.PORT || 3000, '0.0.0.0');
